559af0981e31eae5b2f3c2704c1e035e54e9f962
JackLiuInNC <liu__xian@126.com>
2017-08-14 22:47:41 +0800

    整理代码，增加axios js

--- src/main/webapp/js/index.js
+++
@@ -1,8 +0,0 @@
-String.prototype.trim = function(){
-	var t = this.replace(/(^\s*)|(\s*$)/g,""); 
-	return t.replace(/(^\u3000*)|(\u3000*$)/g,"");
-};
-function killErrors(){
-	return true;
-}
-//window.onerror = killErrors;
